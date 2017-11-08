c0f5db75b1284737def84588a97ce26d289a9702
Levi Zitting <levizitting5663@gmail.com>
2017-11-08 13:27:08 -0600

    Starting fresh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,18 @@
-import { NgModule }         from '@angular/core';
-import { BrowserModule }    from '@angular/platform-browser';
-import { FormsModule }      from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
 
-import { AppComponent }        from './app.component';
-import { DashboardComponent }  from './dashboard.component';
-import { HeroService }         from './hero.service';
-import { HeroesComponent }     from './heroes.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { AppComponent } from './app.component';
 
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-	imports: [
-		BrowserModule,
-		FormsModule,
-		AppRoutingModule,
-		HttpClientInMemoryWebApiModule.forRoot(
-			InMemoryDataService, { dataEncapsulation: false}
-		)
-	],
-	declarations: [
-		AppComponent,
-		HeroDetailComponent,
-		HeroesComponent,
-		DashboardComponent
-	],
-	providers: [
-		HeroService
-	],
-	bootstrap: [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
