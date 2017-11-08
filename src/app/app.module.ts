25e08db4ead9bfe2bb5955b96cda1bb929d8fd7d
Levi Zitting <levizitting5663@gmail.com>
2017-11-07 23:36:09 -0600

    Adding in memory web api to simulate db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { NgModule }         from '@angular/core';
+import { BrowserModule }    from '@angular/platform-browser';
+import { FormsModule }      from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }        from './app.component';
 import { DashboardComponent }  from './dashboard.component';
@@ -14,7 +18,10 @@ import { AppRoutingModule } from './app-routing.module';
 	imports: [
 		BrowserModule,
 		FormsModule,
-		AppRoutingModule
+		AppRoutingModule,
+		HttpClientInMemoryWebApiModule.forRoot(
+			InMemoryDataService, { dataEncapsulation: false}
+		)
 	],
 	declarations: [
 		AppComponent,
