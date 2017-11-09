2789fc17d654715f30b3218c423753135ac5a29e
Levi Zitting <levizitting5663@gmail.com>
2017-11-08 21:07:49 -0600

    Adding ability to add and delete heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,6 +13,10 @@ export class HeroService {
 
 	private heroesUrl = 'api/heroes';
 
+	private httpOptions = {
+		headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+	};
+
 	constructor(
 		private http: HttpClient,
 		private messageService: MessageService
@@ -50,13 +54,26 @@ export class HeroService {
 	}
 
 	updateHero(hero: Hero): Observable<any> {
-		const httpOptions ={
-			headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-		};
-
-		return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+		return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
 			tap(_ => this.log(`Updated hero id=${hero.id}`)),
 			catchError(this.handleError<any>('updateHero'))
+		);
+	}
+
+	addHero(hero: Hero): Observable<Hero> {
+		return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+			tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+			catchError(this.handleError<Hero>('addHero'))
+		);
+	}
+
+	deleteHero(hero: Hero | number): Observable<Hero> {
+		const id = typeof hero === 'number' ? hero : hero.id;
+		const url = `${this.heroesUrl}/${id}`;
+
+		return this.http.delete<Hero>(url, this.httpOptions).pipe(
+			tap(_ => this.log(`Deleted hero id=${id}`)),
+			catchError(this.handleError<Hero>('deletedHero'))
 		);
 	}
 
