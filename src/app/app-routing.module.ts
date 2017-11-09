f34ecf144a9bdd67db58abb3cff2fbe3c23c7581
Levi Zitting <levizitting5663@gmail.com>
2017-11-08 18:48:31 -0600

    Adding root route, which redirects to /dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
 	{ path: 'heroes', component: HeroesComponent },
-	{ path: 'dashboard', component: DashboardComponent }
+	{ path: 'dashboard', component: DashboardComponent },
+	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' }
 ];
 
 @NgModule({
